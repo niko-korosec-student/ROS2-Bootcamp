62556ef9b19cb96aa6321b60f3b55f74d890d985
niko-korosec-student <niko.korosec@student.um.si>
2025-09-12 12:43:30 +0200

    working ros2 env

--- setup_ws.sh
+++ setup_ws.sh
@@ -3,6 +3,6 @@
 source /opt/ros/jazzy/setup.bash
 
 # Source workspace install setup if it exists
-if [ -f /home/ws/install/setup.bash ]; then
-    source /home/ws/install/setup.bash
+if [ -f /workspaces/ROS2-Bootcamp/install/setup.bash ]; then
+    source /workspaces/ROS2-Bootcamp/install/setup.bash
 fi
